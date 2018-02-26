c6e0d19354495206fd09da1431072a45cfecef9a
mreza mnejad <reza.2ta@gmail.com>
2018-02-26 19:01:08 +0330

    asf
    asf

---
+++ IPSC.js
@@ -0,0 +1,9 @@
+// ====================================================================================================
+//
+// Cloud Code for ITSC, write your code here to customize the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://docs.gamesparks.com/
+//
+// ====================================================================================================
+var test=Spark.getPlayerIds();
+var date=GameSparks.getDate();
