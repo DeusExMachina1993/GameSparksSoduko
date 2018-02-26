4c7ac25b20e8b4ee5b9b69548f346984a0ae326d
mreza mnejad <reza.2ta@gmail.com>
2018-02-26 19:01:42 +0330

    Delete IPSC.js

--- IPSC.js
+++
@@ -1,9 +0,0 @@
-// ====================================================================================================
-//
-// Cloud Code for ITSC, write your code here to customize the GameSparks platform.
-//
-// For details of the GameSparks Cloud Code API see https://docs.gamesparks.com/
-//
-// ====================================================================================================
-var test=Spark.getPlayerIds();
-var date=GameSparks.getDate();
