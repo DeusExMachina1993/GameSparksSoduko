0eb3af832898b63f6c9300b331108a8e6d91dcb6
mreza mnejad <reza.2ta@gmail.com>
2018-02-27 18:29:26 +0330

    test
    test

---
+++ event/Grant_Gold.js
@@ -0,0 +1,11 @@
+// ====================================================================================================
+//
+// Cloud Code for Grant_Gold, write your code here to customize the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://docs.gamesparks.com/
+//
+// ====================================================================================================
+var newGold=Spark.getData().gold;
+Spark.getPlayer().credit("gold",newGold, "test");
+Spark.getPlayer().credit2(newGold);
+Spark.setScriptData("data","testing");
